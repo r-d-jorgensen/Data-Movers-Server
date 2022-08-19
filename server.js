dcdade01ba3d1b86611ab8beefff4f062f962528
David <r.d.jorgensen@gmail.com>
2022-08-19 13:45:52 -0700

    changed to pool type for mysql db

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
 
+const pool = require('./sqlDBConnection.js');
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
-
 
 app.listen(process.env.PORT || 4000, () => 
   console.log(`Secure server on port ${process.env.WEB_PORT || 4000}...`)
 );
 
-// TODO: update rds connection to only accept from trusted ip's
-// TODO: update to using a connection pool to limit problems
-const dbConnection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-	database: process.env.DB_DATABASE,
-});
+// check connection to DB
+try {
+  pool.query('SELECT 1 + 1 AS solution', (error, results) => {
+    if (error) {
+      console.log("Bad database Connection");
+      throw error;
+    }else if (results[0].solution === 2) console.log("Connected to database...");
+    else console.log("Something went wrong with database connection");
+  });
+} catch {
+  console.log(error);
+}
 
-// TODO: May need to open and close the connection for each call
-dbConnection.connect((err) => {
-	if (err) {
-		console.error("Database connection failed:\n" + err.stack);
-		return;
-	}
-	console.log("Connected to database.");
-});
-
-app.get('/', (req, res) => {
+app.get('/', (res) => {
 	res.send("This is a help and doc page response");
 });
 
 // return user auth if match
 app.get('/user/login/:username/:password', (req, res) => { 
   const query = "SELECT * FROM users WHERE username = ? AND user_password = ?";
-  dbConnection.query(query, [req.params.username, req.params.password], function (err, result) {
+  pool.query(query, [req.params.username, req.params.password], function (err, result) {
     if (err) {
       res.status(200).json({isAuthed: false, token: "", error: "Server Error"});
       throw err;
@@ -62,7 +55,7 @@ app.get('/user/login/:username/:password', (req, res) => {
 app.get('/user/newUser/:username/:password/:email', (req, res) => {
   // check if username already exists
   const checkUsernameQuery = "SELECT * FROM users WHERE username = ?";
-  dbConnection.query(checkUsernameQuery, [req.params.username, req.params.password], function (err, result) {
+  pool.query(checkUsernameQuery, [req.params.username, req.params.password], function (err, result) {
     if (err) {
       res.status(200).json({isAuthed: false, token: "", error: "Server Error"});
       throw err;
@@ -73,7 +66,7 @@ app.get('/user/newUser/:username/:password/:email', (req, res) => {
       // TODO: fix the nesting of these queries
       // insert new user and return user auth
       const newUserQuery = "INSERT INTO users (username, user_password, email, user_type_ENUM_id) VALUES (?, ?, ?, 3)";
-      dbConnection.query(newUserQuery, [req.params.username, req.params.password, req.params.email], function (err, result) {
+      pool.query(newUserQuery, [req.params.username, req.params.password, req.params.email], function (err, result) {
         if (err) {
           res.status(200).json({isAuthed: false, token: "", error: "Server Error"});
           throw err;
